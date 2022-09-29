0394f8101b61ba8810da52cdc86d9ac62d111e28
Hmsr <hmsrenton@gmail.com>
2022-09-29 11:11:47 +1000

    css change and map component added

--- bus-tracker-app/src/App.js
+++ bus-tracker-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from "./components/Nav.js"
+import Map from "./components/Map.js"
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Map />
       <header className="App-header">
-        <Nav />
         <img src="images/facey.png" className="App-logo" alt="logo" />
         <p>
           Bus Tracker
