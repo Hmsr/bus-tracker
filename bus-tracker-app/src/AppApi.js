d97082f674d70d675b037e07b8809927141cd484
Jordan Junior <jordan.juniorlive.com>
2022-10-22 14:43:29 +1100

    Adding mock buses to AppApi

--- bus-tracker-app/src/AppApi.js
+++ bus-tracker-app/src/AppApi.js
@@ -16,6 +16,39 @@ function postApi(url, content){
 }
 
 export const fetchStationList = fetchApi(`https://stationslist.azurewebsites.net/api/Stations`);
-export const fetchBusList = fetchApi(``);
+export const fetchBusList = fetchMockBuses();
+// export const fetchBusList = fetchApi(``);
 export function postBusArrived(content) { postApi('', content) };
 export function postBusDeparted(content) { postApi('', content) };
+
+
+function fetchMockBuses(){
+    console.log('returning mock buses');
+    return [
+        { busId: 1111, 
+          driverName: "Terry", 
+          lat: -37.8068717725, 
+          long: 144.970312785, 
+          eta: '0:0:10',
+          route: "City Loop",
+          location: "City",
+          occupancy: "Full"}, 
+        { busId: 2222, 
+          driverName: "Eliza", 
+          lat: -37.812717725, 
+          long: 144.956312785, 
+          eta: '0:0:33',
+          route: "Routey Rout",
+          location: "City",
+          occupancy: "Empty"},
+        { busId: 3333, 
+          driverName: "Eric", 
+          lat: -37.82717725, 
+          long: 144.956312785, 
+          eta: '0:3:40',
+          route: "Route 1",
+          location: "City",
+          occupancy: "NearlyEmpty"
+        }
+    ]
+}
--- bus-tracker-app/src/components/StationSelect.js
+++ bus-tracker-app/src/components/StationSelect.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-
-
 export default function StationSelect(props) {
 
     const handleChange = event => {
