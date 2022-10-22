f1de9b975b93fcccdd3426b3938eb10bc056e584
Hamish <103914078@student.swin.edu.au>
2022-10-22 12:30:49 +1100

    details update

--- bus-tracker-app/src/components/Details.js
+++ bus-tracker-app/src/components/Details.js
@@ -3,7 +3,12 @@ import React from "react"
 export default function Details(props) {
     return (
     <div className="card--details">
+    <p>BusID: {props.bus.busId}</p>
     <p>Route: {props.bus.route}</p>
+    <p>ETA: {props.bus.eta}</p>
+    <p>Occupancy: {props.bus.occupancy}</p>
+    <p>Current Location : {props.bus.location}</p>
+    <button>Arrived</button>
     </div>
     
     )
