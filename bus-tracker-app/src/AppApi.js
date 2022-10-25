b9564a0ce6347d5f9cec94246c02f797f23ce548
Jordan Junior <jordan.juniorlive.com>
2022-10-25 14:10:28 +1100

    Updating arrived and departed endpoints

--- bus-tracker-app/src/AppApi.js
+++ bus-tracker-app/src/AppApi.js
@@ -19,8 +19,8 @@ function postApi(url, content){
 export const fetchStationList = fetchApi(`https://stationslist.azurewebsites.net/api/Stations`);
 export const fetchBusList = fetchApi(`https://incomingbuses.azurewebsites.net/api/IncomingBuses/1147`);
 export function fetchBusById(busId) { return fetchApi(`https://incomingbuses.azurewebsites.net/api/IncomingBuses/` + busId) };
-export function postBusArrived(content) { postApi('', content) };
-export function postBusDeparted(content) { postApi('', content) };
+export function postBusArrived(content) { postApi('https://busarrived.azurewebsites.net/api/BusArriveds', content) };
+export function postBusDeparted(content) { postApi('https://busdepartured.azurewebsites.net/api/BusDepartureds', content) };
 
 
 function fetchMockBuses(){
